365e9510baecae975e838643cdae046f4db1e4f0
Elenore Knutsson <elenoreknutsson@gmail.com>
2018-09-20 12:12:46 +0200

    Add SchemaView

---
+++ Teeth/SchemaView.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Teeth
+{
+    partial class SchemaView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.MonthCalendar monthCalendar1;
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDay3 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.btnDay2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnDay1 = new System.Windows.Forms.Button();
+            this.listViewUserDetails = new System.Windows.Forms.ListView();
+            this.listViewTime = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnDay3);
+            this.panel2.Controls.Add(this.listView2);
+            this.panel2.Controls.Add(this.btnDay2);
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Controls.Add(this.btnDay1);
+            this.panel2.Controls.Add(this.listViewUserDetails);
+            this.panel2.Controls.Add(this.listViewTime);
+            this.panel2.Location = new System.Drawing.Point(253, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(747, 686);
+            this.panel2.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.DimGray;
+            this.label10.Location = new System.Drawing.Point(-4, 608);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 24);
+            this.label10.TabIndex = 31;
+            this.label10.Text = "17:00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.DimGray;
+            this.label9.Location = new System.Drawing.Point(-4, 436);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 24);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "14:00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.DimGray;
+            this.label8.Location = new System.Drawing.Point(-4, 491);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 24);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "15:00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DimGray;
+            this.label7.Location = new System.Drawing.Point(-1, 552);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 24);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "16:00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DimGray;
+            this.label6.Location = new System.Drawing.Point(0, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 24);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "11:00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(-1, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 24);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "12:00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DimGray;
+            this.label4.Location = new System.Drawing.Point(-4, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 24);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "13:00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(-1, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "10:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(-4, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "09:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(-1, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "08:00";
+            // 
+            // btnDay3
+            // 
+            this.btnDay3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDay3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDay3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDay3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnDay3.Location = new System.Drawing.Point(528, 3);
+            this.btnDay3.Name = "btnDay3";
+            this.btnDay3.Size = new System.Drawing.Size(213, 31);
+            this.btnDay3.TabIndex = 20;
+            this.btnDay3.Text = "22 Saturday";
+            this.btnDay3.UseVisualStyleBackColor = false;
+            // 
+            // listView2
+            // 
+            this.listView2.AccessibleName = "listViewDay";
+            this.listView2.Font = new System.Drawing.Font("Segoe Script", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(528, 40);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(213, 643);
+            this.listView2.TabIndex = 19;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // btnDay2
+            // 
+            this.btnDay2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDay2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDay2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDay2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnDay2.Location = new System.Drawing.Point(309, 3);
+            this.btnDay2.Name = "btnDay2";
+            this.btnDay2.Size = new System.Drawing.Size(213, 31);
+            this.btnDay2.TabIndex = 18;
+            this.btnDay2.Text = "21 Friday";
+            this.btnDay2.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.AccessibleName = "listViewDay";
+            this.listView1.Font = new System.Drawing.Font("Segoe Script", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(309, 40);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(213, 643);
+            this.listView1.TabIndex = 17;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // btnDay1
+            // 
+            this.btnDay1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDay1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDay1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDay1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnDay1.Location = new System.Drawing.Point(90, 3);
+            this.btnDay1.Name = "btnDay1";
+            this.btnDay1.Size = new System.Drawing.Size(213, 31);
+            this.btnDay1.TabIndex = 16;
+            this.btnDay1.Text = "20 Thursday";
+            this.btnDay1.UseVisualStyleBackColor = false;
+            // 
+            // listViewUserDetails
+            // 
+            this.listViewUserDetails.AccessibleName = "listViewDay1";
+            this.listViewUserDetails.Font = new System.Drawing.Font("Segoe Script", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewUserDetails.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listViewUserDetails.GridLines = true;
+            this.listViewUserDetails.Location = new System.Drawing.Point(90, 40);
+            this.listViewUserDetails.MultiSelect = false;
+            this.listViewUserDetails.Name = "listViewUserDetails";
+            this.listViewUserDetails.Size = new System.Drawing.Size(213, 643);
+            this.listViewUserDetails.TabIndex = 15;
+            this.listViewUserDetails.UseCompatibleStateImageBehavior = false;
+            this.listViewUserDetails.View = System.Windows.Forms.View.Details;
+            // 
+            // listViewTime
+            // 
+            this.listViewTime.AccessibleName = "listViewTime";
+            this.listViewTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listViewTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listViewTime.Enabled = false;
+            this.listViewTime.Font = new System.Drawing.Font("Segoe Script", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listViewTime.GridLines = true;
+            this.listViewTime.Location = new System.Drawing.Point(4, 40);
+            this.listViewTime.MultiSelect = false;
+            this.listViewTime.Name = "listViewTime";
+            this.listViewTime.Size = new System.Drawing.Size(213, 643);
+            this.listViewTime.TabIndex = 21;
+            this.listViewTime.UseCompatibleStateImageBehavior = false;
+            this.listViewTime.View = System.Windows.Forms.View.Details;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(monthCalendar1);
+            this.panel1.Location = new System.Drawing.Point(3, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 686);
+            this.panel1.TabIndex = 13;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.BackColor = System.Drawing.Color.White;
+            monthCalendar1.CalendarDimensions = new System.Drawing.Size(1, 3);
+            monthCalendar1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            monthCalendar1.ForeColor = System.Drawing.Color.Gray;
+            monthCalendar1.Location = new System.Drawing.Point(11, 112);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 0;
+            // 
+            // SchemaView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 719);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "SchemaView";
+            this.Text = "SchemaView";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDay3;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button btnDay2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnDay1;
+        private System.Windows.Forms.ListView listViewUserDetails;
+        private System.Windows.Forms.ListView listViewTime;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
