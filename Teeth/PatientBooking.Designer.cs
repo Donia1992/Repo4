458485377b0435e1293a484d9ccbbc520777d3ef
Totomygoto1 <katta36@hotmail.com>
2018-10-05 13:50:32 +0200

    Added PatientBooking

---
+++ Teeth/PatientBooking.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Teeth
+{
+    partial class PatientBooking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.listView2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Location = new System.Drawing.Point(48, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(809, 745);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(313, 600);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 601);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 600);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Boka in Patient";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(0, 331);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(661, 214);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Inbokade Patienter";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nya Patienter";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(661, 189);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 605);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Förnamn";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(219, 606);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Efternamn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 655);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tandläkare";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 650);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(324, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 701);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Datum";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 696);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(219, 701);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Tid";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(313, 696);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(912, 769);
+            this.Controls.Add(this.panel1);
+            this.Name = "Customer";
+            this.Text = "Customer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Teeth/PatientBooking.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teeth
+{
+
+    public partial class PatientBooking : Form
+    {
+
+        class Patient
+        {
+            public string firstname;
+            public string lastname;
+            public string address;
+            public string zip;
+            public string city;
+        }
+
+        public PatientBooking()
+        {
+            InitializeComponent();
+
+            /*
+            listView1.Items.Add("Patient 1    Anders Nilsson          Gröna gatan 12          333 33    Linköping", 1);
+            listView1.Items.Add("Patient 2    Anna Andersson          Stengatan 5             55 999    Linköping", 2);
+            listView1.Items.Add("Patient 3    Erik Olsson             Trädgårdsvägen 25       11 223    Linköping", 3);
+            listView1.Items.Add("Patient 4    Chen Lee                Stjärnvägen 8           11 223    Linköping", 3);
+            */
+
+            // List Patients
+
+            listView1.View = View.Details;
+            listView1.Columns.Add("Firstname");
+            listView1.Columns.Add("Lastname");           
+            listView1.Columns.Add("Address");
+            listView1.Columns.Add("Zip");
+            listView1.Columns.Add("City");
+
+            List<Patient> patients = new List<Patient>
+            {
+                new Patient() { firstname = "Nils", lastname = "Andersson", address = "Grönagatan 5", zip = "33 333", city = "Linköping" },
+                new Patient() { firstname = "Julia", lastname = "Svensson", address = "Trädgårdsgatan 15", zip = "33 333", city = "Linköping" },
+                new Patient() { firstname = "Chen", lastname = "Lee", address = "Blomkålsgatan 11", zip = "33 333", city = "Linköping" },
+                new Patient() { firstname = "Mike", lastname = "O'Sullivan", address = "Storagatan 8", zip = "33 333", city = "Linköping" }
+            };
+
+            foreach(var p in patients)
+            {
+                ListViewItem lvi1 = new ListViewItem();
+                lvi1.Text = p.firstname.Trim();
+                lvi1.SubItems.Add(p.lastname.Trim());
+                lvi1.SubItems.Add(p.address.Trim());
+                lvi1.SubItems.Add(p.zip.Trim());
+                lvi1.SubItems.Add(p.city.Trim());
+                listView1.Items.Add(lvi1);
+            }
+
+            // Add a Patient
+
+            listView2.View = View.Details;
+            listView2.Columns.Add("Firstname");
+            listView2.Columns.Add("Lastname");          
+            listView2.Columns.Add("Dentist");
+            listView2.Columns.Add("Day");
+            listView2.Columns.Add("Time");
+            
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Customer_FormClosed(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            Program.hem.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi1 = new ListViewItem();
+
+            lvi1.Text = textBox1.Text.Trim();
+            lvi1.SubItems.Add(textBox2.Text.Trim());           
+            lvi1.SubItems.Add(textBox3.Text.Trim());
+            lvi1.SubItems.Add(textBox4.Text.Trim());
+            lvi1.SubItems.Add(textBox5.Text.Trim());
+            
+            listView2.Items.Add(lvi1);
+        }
+    }
+}
