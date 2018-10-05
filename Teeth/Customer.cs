669945b0f74c6fbeb30ce638176e1d20efbd717b
Marcus Nordquist <marcus@queenslab.se>
2018-10-05 13:38:22 +0200

    ♠ GitIgnore fix + added Customer

---
+++ Teeth/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace Teeth
+{
+    public partial class Customer : Form
+    {
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+        private void Cutomer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hide();
+            Home hm = new Home();
+            hm.Show();
+        }
+    }
+}
